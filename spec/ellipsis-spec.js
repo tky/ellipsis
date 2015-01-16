dea3bb2725d3980f34549c00aad2a32aa06207d2
t-murata <takayuki.for.public@gmail.com>
2015-01-16 23:29:14 +0900

    first commit

---
+++ spec/ellipsis-spec.js
@@ -0,0 +1,32 @@
+describe('ellipsis', function() {
+    it('ellipsis simple text', function() {
+      $('#test1').ellipsis({ size: 2 });
+      var result = $('#test1').html();
+      expect('今日...').toBe(result);
+    });
+    it('ellipsis contains strong tag executed before tag', function() {
+      $('#test2').ellipsis({ size: 3 });
+      var result = $('#test2').html();
+      expect('今日は...').toBe(result);
+    });
+    it('ellipsis contains strong tag executed within tag', function() {
+      $('#test3').ellipsis({ size: 4 });
+      var result = $('#test3').html();
+      expect('今日は<strong>金</strong>...').toBe(result);
+    });
+    it('ellipsis contains strong tag executed just over tag', function() {
+      $('#test4').ellipsis({ size: 6 });
+      var result = $('#test4').html();
+      expect('今日は<strong>金曜日</strong>...').toBe(result);
+    });
+    it('ellipsis contains strong tag executed over tag', function() {
+      $('#test5').ellipsis({ size: 7 });
+      var result = $('#test5').html();
+      expect('今日は<strong>金曜日</strong>で...').toBe(result);
+    });
+    it('ellipsis contains 2 tags.', function() {
+      $('#test6').ellipsis({ size: 6 });
+      var result = $('#test6').html();
+      expect('<p>今日は<strong>金曜日</strong></p>...').toBe(result);
+    });
+});
---
+++ src/ellipsis.js
@@ -0,0 +1,79 @@
+(function($) {
+  var StringIterator = function(html) {
+    this.html = html;
+    this.length = html.length;
+    this.index = 0;
+    this.tags = [];
+
+    this.countClosedTagChar = function() {
+      for (var i = 1; i < this.length; i++) {
+        if (this.html[this.index + i] === '>') {
+          return i + 1;
+        }
+      }
+      throw new Error('tag is not closed.');
+    }
+    this.isCloseTag = function(tag) {
+      return tag[1] === '/';
+    }
+    this.toCloseTag = function(openTag) {
+      return openTag.substr(0, 1) + '/' + openTag.substr(1);
+    }
+  };
+
+  StringIterator.prototype.next = function() {
+    var next = this.html[this.index];
+    if (!next) {
+      return null;
+    } else if (next === '<') {
+      var pos = this.countClosedTagChar();
+      var tag = this.html.slice(this.index, this.index + pos);
+      if (this.isCloseTag(tag)) {
+        this.tags.pop();
+      } else {
+        this.tags.push(this.toCloseTag(tag));
+      }
+      this.index += pos;
+      return { value: tag, tag: true};
+    } else {
+      this.index++;
+      return { value: next, tag: false};
+    }
+  };
+
+  StringIterator.prototype.popAllCloseTags = function() {
+    return this.tags.join('');
+  };
+  $.fn.ellipsis = function(options) {
+    var settings = $.extend({
+      size: 10,
+      omission: '...',
+    }, options);
+
+    return this.each(function() {
+      var self = $(this);
+      var init = function() {
+        self.each(function() {
+          var ite = new StringIterator($(this).html());
+          var i = ite.next();
+          var ellipsised = "";
+          var textSize = 0;
+          while (i) {
+            ellipsised += i.value;
+            if (!i.tag) {
+              textSize++;
+            }
+            if (textSize >= settings.size) {
+              ellipsised += ite.popAllCloseTags();
+              break;
+            }
+            i = ite.next();
+          }
+          $(this).html(ellipsised + settings.omission);
+        });
+      };
+      init();
+    });
+  };
+})(jQuery);
+
