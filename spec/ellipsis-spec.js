0a06fd0f69a1ab4791a1ab686c9ec285d9120666
t-murata <takayuki.for.public@gmail.com>
2015-01-25 12:38:17 +0900

    refactor specs.

--- spec/ellipsis-spec.js
+++ spec/ellipsis-spec.js
@@ -1,37 +1,51 @@
 describe('ellipsis', function() {
     it('ellipsis simple text', function() {
-      $('#test1').ellipsis({ size: 2 });
-      var result = $('#test1').html();
+      var target = $('#text');
+      target.html('今日は金曜日です。');
+      target.ellipsis({ size: 2 });
+      var result = target.html();
       expect('今日...').toBe(result);
     });
     it('ellipsis contains strong tag executed before tag', function() {
-      $('#test2').ellipsis({ size: 3 });
-      var result = $('#test2').html();
+      var target = $('#text');
+      target.html('今日は<strong>金曜日</strong>です。');
+      target.ellipsis({ size: 3 });
+      var result = target.html();
       expect('今日は...').toBe(result);
     });
     it('ellipsis contains strong tag executed within tag', function() {
-      $('#test3').ellipsis({ size: 4 });
-      var result = $('#test3').html();
+      var target = $('#text');
+      target.html('今日は<strong>金曜日</strong>です。');
+      target.ellipsis({ size: 4 });
+      var result = target.html();
       expect('今日は<strong>金</strong>...').toBe(result);
     });
     it('ellipsis contains strong tag executed just over tag', function() {
-      $('#test4').ellipsis({ size: 6 });
-      var result = $('#test4').html();
+      var target = $('#text');
+      target.html('今日は<strong>金曜日</strong>です。');
+      target.ellipsis({ size: 6 });
+      var result = target.html();
       expect('今日は<strong>金曜日</strong>...').toBe(result);
     });
     it('ellipsis contains strong tag executed over tag', function() {
-      $('#test5').ellipsis({ size: 7 });
-      var result = $('#test5').html();
+      var target = $('#text');
+      target.html('今日は<strong>金曜日</strong>です。');
+      target.ellipsis({ size: 7 });
+      var result = target.html();
       expect('今日は<strong>金曜日</strong>で...').toBe(result);
     });
     it('ellipsis contains 2 tags.', function() {
-      $('#test6').ellipsis({ size: 6 });
-      var result = $('#test6').html();
+      var target = $('#text');
+      target.html('<p>今日は<strong>金曜日</strong>です。</p>');
+      target.ellipsis({ size: 6 });
+      var result = target.html();
       expect('<p>今日は<strong>金曜日</strong></p>...').toBe(result);
     });
     it('ellipsis size is from data-ellipsis-length', function() {
-      $('#test7').ellipsis();
-      var result = $('#test7').html();
+      var target = $('#text-with-length');
+      target.html('今日は金曜日です。');
+      target.ellipsis();
+      var result = target.html();
       expect('今...').toBe(result);
     });
 });
